3481f5b2d362ee9267b4e06cd24ccf8e22eda025
bryangodson <ppdnquh@gmail.com>
2025-01-16 11:06:03 +0000

    dir:ignored the out folder

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
+const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
-  //   output: "export",
-  //   basePath: "/tel",
-  //   trailingSlash: true,
+  output: "export",
+  basePath: isProd ? "/tel" : "",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: isProd ? "/tel" : "",
 };
 
 export default nextConfig;
